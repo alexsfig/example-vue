c73d6036ed3990eb23bfe96fbe2360a84aebec58
Milton Alexander Sosa <alexandersosa2204@gmail.com>
2017-10-11 16:20:25 -0600

    change views to new format

--- src/main.js
+++ src/main.js
@@ -2,14 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import CustomHead from './Header'
 import router from './router'
+import moment from 'moment'
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('YYYY/MM/DD')
+  }
+})
+Vue.filter('formatDateTime', function(value) {
+  if (value) {
+    return moment(String(value)).format('YYYY/MM/DD/ hh:mm')
+  }
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 import VeeValidate from 'vee-validate';
-
+Vue.component('modal', {
+  template: '#modal'
+})
 Vue.use(VeeValidate);
 new Vue({
   el: '#app',
@@ -17,14 +30,4 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
-new Vue({
-  el: '#CustomHead',
-  router,
-  template: '<CustomHead/>',
-  components: { CustomHead },
-  //  mounted: function () {
-  //   $(document).ready(function () {
-  //     $('#sel').material_select()
-  //   })
-  // }
-})
+
