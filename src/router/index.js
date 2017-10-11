2b607e33bfaac079e462b9dadf0b23299ae4215e
Milton Alexander Sosa <alexandersosa2204@gmail.com>
2017-10-11 16:19:47 -0600

    update routers use custom components

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,9 @@ import Coins from '@/components/Coins'
 import EmployeesCreate from '@/components/employees/Create'
 import EmployeesShow from '@/components/employees/Show'
 import EmployeeIndex from '@/components/employees/Index'
-const User = {
-  template: `
-  <h1>ssss </h1>
-  `
-}
+import AdminTemplate from '@/layouts/Admin'
+import LoginTemplate from '@/layouts/Login'
+
 Vue.use(Router)
 export default new Router({
   mode: 'history',
@@ -29,8 +27,14 @@ export default new Router({
     },
     {
       path: '/login',
-      name: 'Login',
-      component: Login
+      component: LoginTemplate,
+      children:[
+        {
+          path: '',
+          name: 'Login',
+          component: Login
+        }
+      ]
     },
     {
       path: '/coins/:id',
@@ -44,20 +48,20 @@ export default new Router({
     },
     {
         path: '/admin',
-        component: User,
+        component: AdminTemplate,
         children: [
             {
-              path: '/employees/create',
+              path: 'employees/create',
               name: 'EmployeesCreate',
               component: EmployeesCreate
             },
             {
-              path: '/employees',
+              path: 'employees',
               name: 'EmployeeIndex',
               component: EmployeeIndex
             },
             {
-              path: '/employees/:id',
+              path: 'employees/:id',
               name: 'EmployeesShow',
               component: EmployeesShow
             }
