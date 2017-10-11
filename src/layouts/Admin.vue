2788964d2ce45d9bb4b8f29aef658d3654dc5747
Milton Alexander Sosa <alexandersosa2204@gmail.com>
2017-10-11 14:48:21 -0600

    add layaouts

---
+++ src/layouts/Admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav>
+      <div class="nav-wrapper">
+          <a href="#" class="brand-logo right">Logo</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+              <li><router-link to="/login">Login</router-link></li>
+              <li><router-link to="/coins/bitcoin">BitCoin</router-link></li>
+              <li><router-link to="/coins">Coins</router-link></li>
+          </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view/>
+      
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
